09dc52c5fa49a039d8ad7bf427b04901c0a5eb1a
delwer72 <mddelwerhussain93@gmail.com>
2026-06-11 17:11:38 +0600

    updated

--- src/proxy.js
+++ src/proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export function middleware(request) {
+export function proxy(request) {
   // Better Auth session token check
   const token = request.cookies.get("better-auth.session_token");
 
